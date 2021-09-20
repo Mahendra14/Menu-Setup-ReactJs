d794b7b817ef568add2834906a6b0cbae53f2ac0
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-20 13:21:36 +0530

    Added functionality for manual overriding of buttons

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import items from './data';
 function App() {
   const [menuItems,setMenuItems] = useState(items);
   const [categories,setCategories] = useState([]);
-
+  
+  //for sake of manual updating the data
+  const filterItems = (catergory) => {
+    if(catergory === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === catergory);
+    setMenuItems(newItems);
+  }
   return <main>
     <section class="menu section">
       <div class="title">
       <h2>Our Menu</h2>
       <div class="underline"></div>
       </div>
-      <Categories />
+      <Categories filterItems = {filterItems}/>
       <Menu items={menuItems}/>
 
     </section>
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems}) => {
+  return <div class="btn-container">
+    <button class="filter-btn" onClick = {() => {
+      filterItems('all');
+    }}>
+      all
+    </button>
+    <button class="filter-btn" onClick = {() => {
+      filterItems('breakfast');
+    }}>
+      breakfast
+    </button>
+  </div>;
 };
 
 export default Categories;
